a9f30c1f61cf3d0a2c0485996164ab056c944df8
ErayHerguel <115701646+ErayHerguel@users.noreply.github.com>
2022-11-08 09:01:24 +0100

    Add files via upload

---
+++ ampel.js
@@ -0,0 +1,18 @@
+let licht = "rot";
+
+if(licht == "rot")
+{
+    console.log("Rot");
+}
+else if(licht == "orange")
+{
+    console.log("Orange");
+}
+else if(licht == "grün")
+{
+    console.log("Grün");
+}
+else
+{
+    console.log("Die Ampel ist ausgefallen!");
+}
