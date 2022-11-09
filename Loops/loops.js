2af9c50994d8b2ab3c08c0da8c4ab8553edbd764
ErayHerguel <eray.herguel@web.de>
2022-11-09 18:48:43 +0100

    Task 1

--- Loops/loops.js
+++ Loops/loops.js
@@ -1,5 +1,12 @@
 // Task 1: Create a function to console.log all the numbers from 0 to 10 using while loop OR for loop
 
+let x = 0;
+
+while (x <= 10)
+{
+    console.log(x);
+    x++;
+}
 
 // Task 2: Create a function that takes in an array of numbers as an argument and console.log only even numbers (use for loop)
 // const array = [1,4,5,6,7]
