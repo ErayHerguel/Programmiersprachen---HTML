fee8a652418fe7bb981320ff3fa2f33a145e5ae4
ErayHerguel <eray.herguel@web.de>
2022-11-09 18:44:42 +0100

    Add Loops

---
+++ Loops/loops.js
@@ -0,0 +1,10 @@
+// Task 1: Create a function to console.log all the numbers from 0 to 10 using while loop OR for loop
+
+
+// Task 2: Create a function that takes in an array of numbers as an argument and console.log only even numbers (use for loop)
+// const array = [1,4,5,6,7]
+// 4,6
+
+// Task 3: Create a function that takes in an array of numbers and console.log the array in the reverse order
+// const array=[1,2,3,4]
+// const reversedArray=[4,3,2,1]
