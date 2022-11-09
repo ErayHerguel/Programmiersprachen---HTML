38523bc678188c99bd141c4ff191bc05e5889177
ErayHerguel <eray.herguel@web.de>
2022-11-09 19:12:09 +0100

    Task 3 + Update Task 2

--- Loops/loops.js
+++ Loops/loops.js
@@ -12,17 +12,29 @@
 // const array = [1,4,5,6,7]
 // 4,6
 
-const array = [1, 4, 5, 6, 7];
-let x = 0;
+// const array = [1, 4, 5, 6, 7];
+// let x = 0;
 
-for(let i = 0; i <= 10; i++)
-{
-    if(array[i] % 2 == 0)
-    {
-        console.log(array[i]);
-    }
-}
+// for(let i = 0; i < array.length; i++)
+// {
+//     if(array[i] % 2 == 0)
+//     {
+//         console.log(array[i]);
+//     }
+// }
 
 // Task 3: Create a function that takes in an array of numbers and console.log the array in the reverse order
 // const array=[1,2,3,4]
 // const reversedArray=[4,3,2,1]
+
+const array = [1, 2, 3, 4];
+const reversedarray = [4, 3, 2, 1];
+
+for(let i = 0; i < array.length; i++)
+{
+        console.log(array[i]);
+}
+for(let i = 0; i < array.length; i++)
+{
+        console.log(reversedarray[i]);
+}
