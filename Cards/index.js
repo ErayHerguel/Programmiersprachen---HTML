6d93f73ff22c95af0b88a6b3766fa977b6bb62ce
ErayHerguel <eray.herguel@web.de>
2022-11-16 18:55:34 +0100

    Update index.js

--- Cards/index.js
+++ Cards/index.js
@@ -1,9 +1,18 @@
+// parent element - div id
 //selecting an element
 const cardsContainer = document.getElementById('cards-container')
+const addButton = document.getElementById('add-btn')
+const removeButton = document.getElementById('remove-btn')
+// e.g. for class
 // document.querySelector('.some-className')
 
 const createCards = () => 
 {
+    if (cardsContainer.hasChildNodes())
+    {
+        return;
+    }
+
     for (let i=0; i < 5; i++)
     {
         // create an element(
@@ -12,9 +21,27 @@ const createCards = () =>
         card.innerHTML = `<p>Element number ${i + 1}</p>`
         // add styles - class
         card.classList.add('card')
+        // append element
         cardsContainer.appendChild(card)
-
     }
 }
 
-createCards();
+const removeCards = () => 
+{
+    // check if cardsContainer has child elements
+    if (cardsContainer.hasChildNodes())
+    {
+        //remove child elements
+        while(cardsContainer.firstChild)
+        {
+            cardsContainer.removeChild(cardsContainer.firstChild)
+        }
+    }
+}
+
+// createCards();
+
+// addEventListener - attaches an event listener to an element
+// element.addEventListener(event, function)
+addButton.addEventListener('click', createCards)
+removeButton.addEventListener('click', removeCards)
